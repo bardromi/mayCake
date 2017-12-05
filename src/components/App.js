76eb80ab62474d19fbc43e9feda4fb537f7fa426
Bar <bardromi@gmail.com>
2017-12-05 23:17:01 +0200

    Added navbar template

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react'
+import Nav  from './Nav'
 
 export default class App extends Component {
     render() {
         return (
             <div>
-                <h1>Hello World!</h1>
+                <Nav/>
             </div>
         )
     }
