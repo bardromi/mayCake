eedcef3898b2f42124bd1919a745ace5253aeeb0
Bar Dromi <bardromi@gmail.com>
2018-03-05 18:30:19 +0200

    added likes and cakes names

--- src/api.js
+++ src/api.js
@@ -42,49 +42,57 @@ export const CakesAPI = {
             id: 1,
             category: "cake",
             name: "Chocolate Cake",
-            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAtutm6NaoBbTY5Vli75zaAWwwDKf7bfPecSBEhMHCvdSS3dJx"
+            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAtutm6NaoBbTY5Vli75zaAWwwDKf7bfPecSBEhMHCvdSS3dJx",
+            likes: 1
         },
         {
             id: 2,
             category: "cake",
             name: "Cheese Cake",
-            img: "http://img.taste.com.au/JykbG0mr/w643-h428-cfill-q90/taste/2016/11/new-york-cheesecake-40742-1.jpeg"
+            img: "http://img.taste.com.au/JykbG0mr/w643-h428-cfill-q90/taste/2016/11/new-york-cheesecake-40742-1.jpeg",
+            likes:22
         },
         {
             id: 3,
             category: "cake",
             name: "Black Cake",
-            img: "https://celebdear.com/wp-content/uploads/2017/01/0020236_choco_velvette_cake-2.jpeg"
+            img: "https://celebdear.com/wp-content/uploads/2017/01/0020236_choco_velvette_cake-2.jpeg",
+            likes: 123
         },
         {
             id: 4,
             category: "cake",
             name: "White Cake",
-            img: "http://images.media-allrecipes.com/userphotos/960x960/4482234.jpg"
+            img: "http://images.media-allrecipes.com/userphotos/960x960/4482234.jpg",
+            likes: 111
         },
         {
             id: 5,
             category: "cake",
             name: "Oreo Cake",
-            img: "http://bakingbites.com/wp-content/uploads/2009/04/cookiecake.jpg"
+            img: "http://bakingbites.com/wp-content/uploads/2009/04/cookiecake.jpg",
+            likes: 69
         },
         {
             id: 6,
             category: "cake",
             name: "Rainbow Cake",
-            img: "https://www.fergusonplarre.com.au/media/catalog/product/cache/1/image/550x/9df78eab33525d08d6e5fb8d27136e95/b/i/birthday-pinata-cake--inside.jpg"
+            img: "https://www.fergusonplarre.com.au/media/catalog/product/cache/1/image/550x/9df78eab33525d08d6e5fb8d27136e95/b/i/birthday-pinata-cake--inside.jpg",
+            likes: 2
         },
         {
             id: 7,
             category: "cake",
             name: "Car Cake",
-            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy0hYOjFHo5boh_0ULNtd4KG4Je_308pJUJk_505wmgTn8YkPU"
+            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy0hYOjFHo5boh_0ULNtd4KG4Je_308pJUJk_505wmgTn8YkPU",
+            likes: 10
         },
         {
             id: 8,
             category: "cake",
             name: "Heart Cake",
-            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJCyKc8egRAPnAHDqG44hzsnY72YDR20atic_RdTuApAecxaVz"
+            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJCyKc8egRAPnAHDqG44hzsnY72YDR20atic_RdTuApAecxaVz",
+            likes: 1000
         },
     ],
     all: function(){
--- src/components/CakeCard.js
+++ src/components/CakeCard.js
@@ -4,8 +4,8 @@ const CakeCard = (props) => {
     return (
         <div className="col-md-3">
             <div className="card mb-5">
-                <h5 className="card-title text-center pt-2">Chocolate Cake</h5>
-
+                <h5 className="card-title text-center pt-2">{props.cake.name}</h5>
+                <hr/>
                 <a href="#">
                     <img className="card-img-top ml-1"
                          src={props.cake.img}
@@ -14,7 +14,7 @@ const CakeCard = (props) => {
 
                 <div className="card-body">
                     <i className="far fa-thumbs-up fa-lg fa-pull-right"></i>
-                    <span>10 likes</span>
+                    <span>{props.cake.likes}</span>
                 </div>
             </div>
         </div>
